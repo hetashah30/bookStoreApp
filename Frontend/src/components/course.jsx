b4e92a8291cbd4ea647e9c0fba3f90813fb9c32a
Heta Shah <144616306+hetashah30@users.noreply.github.com>
2025-07-21 11:59:40 +0530

    Update course.jsx

--- Frontend/src/components/course.jsx
+++ Frontend/src/components/course.jsx
@@ -1,61 +1,88 @@
 import React, { useState, useEffect } from "react";
-import Cards from "./cards"; // Importing the Cards component
-// import list from "../assets/list.json"; // Importing the course list - this is used only for testing purposes, actual data will be fetched from the POSTMAN API in the backend by using book.map instead of list.map
-import { Link } from "react-router-dom"; // Importing Link for navigation
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+// import list from "../assets/list.json"; // Importing the book list - this is used only for testing purposes, actual data will be fetched from the POSTMAN API in the backend by using book.map instead of list.map
 import axios from "axios"; // Importing axios for API calls
+import Cards from "./cards"; // Importing the Cards component
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL; // Using environment variable for the base URL
 
-function Course() {
+function Freebook() {
   const [book, setBook] = useState([]); // State to hold book data from the API (if no books - initially empty array to avoid errors)
   useEffect(() => {
     //Fetching book data from the API
     const getBook = async () => {
       try {
         const res = await axios.get(`${baseURL}/book`);
-        console.log(res.data);
-        setBook(res.data);
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
       } catch (error) {
         console.log(error);
       }
     };
     getBook();
   }, []);
+  // const filterData = list.filter((data) => data.category === "Free");
+  // console.log(filterData);
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  };
+
   return (
     <>
-      {/* <div className="max-w-screen-2xl container mx-auto px-4 sm:px-6 lg:px-10"> */}
-      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
-        <div className="mt-20 items-center justify-center text-center">
-          <h1 className="text-2xl md:text-4xl text-center mt-20 mb-12">
-            We're delighted to have you{" "}
-            <span className="text-pink-500">here :)</span>
+      <div
+        id="free"
+        className="w-full mt-20 mb-20 container mx-auto px-4 sm:px-6 lg:px-10"
+      >
+        <div>
+          <h1 className="pb-2 text-lg sm:text-xl font-semibold">
+            Free Offered Books
           </h1>
           <p>
-            Welcome to Your Book Library! Explore, Read, Learn, and Grow with
-            our curated books. Explore, Read, Learn, and Grow with our curated
-            collection of books.
-            <br />
-            Whether you're here for adventure, knowledge, or inspiration – we've
-            got the perfect book for you!
-            <br />
-            🚀 Ready to get started? Browse our books below and begin your
-            reading adventure!
+            Explore our specially curated selection of free books designed to
+            boost your knowledge and skills—at zero cost!
           </p>
-          <Link to="/">
-            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 transition-colors duration-300">
-              Back
-            </button>
-          </Link>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
-          {book.map((item) => (
-            <Cards key={item._id} item={item} />
-          ))}
+        <div className="w-full mt-4 custom-slider flex flex-col gap-4">
+          {/* Using the Slider component to display the cards. To pass the data from parents to child we use props. Props: In react "props" (properties) are a way to pass data from a parent component to a child component. Prop allows, component to be dynamic and render accordingly. */}
+          <Slider {...settings}>
+            {book.map((item) => (
+              <div key={item._id}>
+                <Cards item={item} />
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </>
   );
 }
 
-export default Course;
+export default Freebook;
